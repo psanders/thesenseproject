9f676640bfd4b3380d062fadad278d25c6e3f583
Pedro Sanders <sanderspedro@gmail.com>
2014-09-29 01:30:14 -0400

    Update fsync.py

--- fsync.py
+++ fsync.py
@@ -2,8 +2,7 @@ import serial
 import time
 import array
 
-RETRY = 3
-# AT CMDS
+# AT cmds to initiate modem
 CMD_AT = ["AT","OK"]  # First test if everything is okay.
 CMD_AT_CPIN = ["AT+CPIN?", "+CPIN: READY"]  # This is to check if SIM is unlocked.
 CMD_AT_CREG = ["AT+CREG?", "+CREG:"] # This checks if SIM is registered or not
@@ -15,6 +14,16 @@ CMD_AT_CSTT = ["AT+CSTT=\"fast.t-mobile\",\" \",\" \"", "OK"] # Start the task, 
 CMD_AT_CIICR = ["AT+CIICR", "OK"] # Bring wireless up
 CMD_AT_CIFSR = ["AT+CIFSR", "."] # Get the local IP address.
 INIT_SEC = [CMD_AT, CMD_AT_CPIN, CMD_AT_CREG, CMD_AT_CGATT, CMD_AT_CIPSHUT, CMD_AT_CIPSTATUS, CMD_AT_CIPMUX, CMD_AT_CSTT, CMD_AT_CIICR, CMD_AT_CIFSR]
+# AT cmds to setup ftp client 
+CMD_AT_FTPSERV = ["AT+FTPSERV=\"phonytive.com\"", "OK"]
+CMD_AT_FTPPORT = ["AT+FTPPORT=21", "OK"]
+CMD_AT_FTPPUN = ["AT+FTPUN=\"wwrsftp\"", "OK"]
+CMD_AT_FTPPW = ["AT+FTPPW=\"G4t0p4rd0#\"", "OK"]
+CMD_AT_FTPPUTNAME = ["AT+FTPPUTNAME=", "OK"] # You must add the file name !!!
+CMD_AT_FTPPUTPATH = ["AT+FTPPUTPATH=\"/wwrsftp\"", "OK"]
+CMD_AT_FTPPUT = ["AT+FTPPUT=1", "OK"]
+FTP_SEC = [CMD_AT_FTPSERV, CMD_AT_FTPPORT, CMD_AT_FTPPUN, CMD_AT_FTPPW, CMD_AT_FTPPUTPATH]
+
 
 # Sim900 modem
 port = serial.Serial("/dev/ttyAMA0", baudrate = 115200, timeout = 10)
@@ -38,30 +47,29 @@ def send_cmd(cmd):
 
   if cmd[1] in response:
     return response
+  # Debugging response
+  print response
   return 0
-
-def is_connection_alive():
-  response = send_cmd(CMD_AT_CIPSTATUS) 
-  print(response)
-  if send_cmd(CMD_AT_CIPSTATUS) is not 0:
-    return True
-  return False
 
 # Initialize the modem
 def init():
-  for i in INIT_SEC:
-    response = send_cmd(i);
-    print(response);
+    for i in INIT_SEC:
+        response = send_cmd(i);
+        print(response);
+    
+def setup_ftp():
+    for i in FTP_SEC:
+        response = send_cmd(i);
+        print(response);
+def upload_ftp(file):
+    CMD_AT_FTPPUTNAME[0] = CMD_AT_FTPPUTNAME + "\"" + file "\""
+    response = send_cmd(CMD_AT_FTPPUTNAME)
+    print response
+    
+    
+init()
+setup_ftp()
     
 while True:
-  init()
-  if is_connection_alive():
-    # Try to connect to a random server
-    cmd = ["AT+CIPSTART=\"TCP\",\"astivetoolkit.org\",\"80\"", ""]
-    response = send_cmd(cmd);
-    print(response)
-    print(is_connection_alive())
-  else:
-    init()
-  # Check again in 60 secs
-  time.sleep(30000)
+
+    time.sleep(5000)
