f7d29a2e2ed0d2a1045887019fc579a22dc9cb70
Pedro Sanders <sanderspedro@gmail.com>
2014-10-05 18:15:09 -0400

    Create common.py

---
+++ common.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import RPi.GPIO as GPIO
+import time
+import atio
+import atftp
+
+POWER_PIN = 12
+CMD_AT = ["AT","OK"]
+
+def toggle_button(on):
+    if on != is_power_on():
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(POWER_PIN, GPIO.OUT)
+        GPIO.output(POWER_PIN, True)
+        time.sleep(2)
+        GPIO.output(POWER_PIN, False)
+
+def is_power_on():
+    response = atio.send_cmd(CMD_AT, 0.5)
+    if response is not False:
+        return True
+    return False
