42824b8255990347d7644247f1963ad0e49c6335
Pedro Sanders <sanderspedro@gmail.com>
2014-10-05 18:17:27 -0400

    Create atio.py

---
+++ atio.py
@@ -0,0 +1,23 @@
+# Basic I/O operations on sim900 modem
+import serial
+import time
+
+# Sim900 modem
+port = serial.Serial(port='/dev/ttyAMA0', baudrate=9600, timeout=0.5, writeTimeout=2)
+
+def read():
+    return port.read(1024)
+
+# Basic I/O operation: Read
+def write(str):
+    port.write(str + "\r\n")
+
+# Send a cmd an return a response or 0 in case off error
+def send_cmd(cmd, timeout):
+  write(cmd[0])
+  time.sleep(timeout)
+  response = read()
+  print "{", response, "}"
+  if cmd[1] in response:
+    return response
+  return False
